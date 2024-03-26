dad1a005ec028f2adb7011f77e6ad74e80fdffa7
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-03-27 00:44:35 +0600

    basic setup

---
+++ src/compnents/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>hello world home</div>;
+};
+
+export default Home;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Root from "./compnents/Root/Root";
+import "./index.css";
+import Home from "./compnents/Home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
