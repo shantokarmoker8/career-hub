17af7a77aa14361429c943b8ad66666a7184b0a8
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-03-27 01:21:36 +0600

    navber

--- src/compnents/Root/Root.jsx
+++ src/compnents/Root/Root.jsx
@@ -1,8 +1,10 @@
+import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 const Root = () => {
   return (
     <div>
+      <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
     </div>
